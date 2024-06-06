9c6bb7da7d9c725a36ec47b88804abd475911116
wcz <hwyrqw@163.com>
2024-06-07 00:26:16 +0800

    deploy.sh docker

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -20,53 +20,53 @@ works=$(git log -1 --stat |awk -F ' ' '{print $1}' |awk -F '/' '{print $1}' | aw
 echo "works: "
 echo $works
 #java
-#if [ "$(echo $works|grep '#wccloud-admin#')" != "" ]; then
-#  echo '##########开始构建wccloud-admin##########'
-#  package
-#  if [ "$(docker ps|grep wccloud-admin)" != "" ]; then
-#    echo  'stop rm rmi '
-#   docker stop wccloud-admin && docker rm wccloud-admin && docker rmi wccloud-admin
-#  fi
-#  docker compose -f wccloud-admin.yml up -d
-#fi
-#
-#if [ "$(echo $works|grep '#wccloud-auth#')" != "" ]; then
-#  echo '##########开始构建wccloud-auth##########'
-#  package
-#  if [ "$(docker ps|grep wccloud-auth)" != "" ]; then
-#    echo  'stop rm rmi '
-#   docker stop wccloud-auth && docker rm wccloud-auth && docker rmi wccloud-auth
-#  fi
-#  docker compose -f wccloud-auth.yml up -d
-#fi
-#
-#if [ "$(echo $works|grep '#wccloud-gateway#')" != "" ]; then
-#  echo '##########开始构建wccloud-gateway##########'
-#  package
-#  if [ "$(docker ps|grep wccloud-gateway)" != "" ]; then
-#    echo  'stop rm rmi '
-#   docker stop wccloud-gateway && docker rm wccloud-gateway && docker rmi wccloud-gateway
-#  fi
-#  docker compose -f wccloud-gateway.yml up -d
-#fi
-#
-##rust
-#if [ "$(echo $works|grep '#wccloud-web-rust#')" != "" ]; then
-#  echo '##########开始构建wccloud-web-rust##########'
-#  cd ..
-#  cd wccloud-web-rust || exit
-#  #/root/.cargo/bin/cargo  build --release
-#  if [ "$(docker ps|grep wccloud-web-rust)" != "" ]; then
-#    echo  'stop rm rmi '
-#   docker stop wccloud-web-rust && docker rm wccloud-web-rust && docker rmi wccloud-web-rust
-#  fi
-#  cd ..
-#  cd deploy || exit
-#  docker compose -f wccloud-web-rust.yml up -d
-#  #将docker内构建的依赖以及缓存复制到 jenkins内的项目目录，提升下次编译速度
-#  docker cp  wccloud-web-rust:/usr/local/cargo /var/jenkins_home/workspace/wccloud/wccloud-web-rust/
-#  docker cp  wccloud-web-rust:/wccloud-web-rust/target /var/jenkins_home/workspace/wccloud/wccloud-web-rust/
-#fi
+if [ "$(echo $works|grep '#wccloud-admin#')" != "" ]; then
+  echo '##########开始构建wccloud-admin##########'
+  package
+  if [ "$(docker ps|grep wccloud-admin)" != "" ]; then
+    echo  'stop rm rmi '
+   docker stop wccloud-admin && docker rm wccloud-admin && docker rmi wccloud-admin
+  fi
+  docker compose -f wccloud-admin.yml up -d
+fi
+
+if [ "$(echo $works|grep '#wccloud-auth#')" != "" ]; then
+  echo '##########开始构建wccloud-auth##########'
+  package
+  if [ "$(docker ps|grep wccloud-auth)" != "" ]; then
+    echo  'stop rm rmi '
+   docker stop wccloud-auth && docker rm wccloud-auth && docker rmi wccloud-auth
+  fi
+  docker compose -f wccloud-auth.yml up -d
+fi
+
+if [ "$(echo $works|grep '#wccloud-gateway#')" != "" ]; then
+  echo '##########开始构建wccloud-gateway##########'
+  package
+  if [ "$(docker ps|grep wccloud-gateway)" != "" ]; then
+    echo  'stop rm rmi '
+   docker stop wccloud-gateway && docker rm wccloud-gateway && docker rmi wccloud-gateway
+  fi
+  docker compose -f wccloud-gateway.yml up -d
+fi
+
+#rust
+if [ "$(echo $works|grep '#wccloud-web-rust#')" != "" ]; then
+  echo '##########开始构建wccloud-web-rust##########'
+  cd ..
+  cd wccloud-web-rust || exit
+  #/root/.cargo/bin/cargo  build --release
+  if [ "$(docker ps|grep wccloud-web-rust)" != "" ]; then
+    echo  'stop rm rmi '
+   docker stop wccloud-web-rust && docker rm wccloud-web-rust && docker rmi wccloud-web-rust
+  fi
+  cd ..
+  cd deploy || exit
+  docker compose -f wccloud-web-rust.yml up -d
+  #将docker内构建的依赖以及缓存复制到 jenkins内的项目目录，提升下次编译速度
+  docker cp  wccloud-web-rust:/usr/local/cargo /var/jenkins_home/workspace/wccloud/wccloud-web-rust/
+  docker cp  wccloud-web-rust:/wccloud-web-rust/target /var/jenkins_home/workspace/wccloud/wccloud-web-rust/
+fi
 
 #node
 #配置npm加速代理
@@ -74,24 +74,24 @@ npm config set registry https://registry.npmmirror.com
 npm -version
 node --version
 
-##if [ "$(echo $works|grep 'nuxt_admin')" != '' ]; then
-#  echo '##########开始构建nuxt_admin##########'
-#  cd ..
-#  cd nuxt_admin || exit
-#  pwd
-#  echo 'npm install...'
-#  npm install
-#  echo 'npm run build...'
-#  npm run build
-#  if [ "$(docker ps|grep nuxt_admin)" != "" ]; then
-#   docker stop nuxt_admin && docker rm nuxt_admin && docker rmi nuxt_admin
-#  fi
-#  cd ..
-#  cd deploy || exit
-#  echo 'docker compose -f nuxt_admin.yml up -d'
-#  docker compose -f nuxt_admin.yml up -d
-##fi
-#if [ "$(echo $works|grep 'nuxt_web')" != '' ]; then
+if [ "$(echo $works|grep 'nuxt_admin')" != '' ]; then
+  echo '##########开始构建nuxt_admin##########'
+  cd ..
+  cd nuxt_admin || exit
+  pwd
+  echo 'npm install...'
+  npm install
+  echo 'npm run build...'
+  npm run build
+  if [ "$(docker ps|grep nuxt_admin)" != "" ]; then
+   docker stop nuxt_admin && docker rm nuxt_admin && docker rmi nuxt_admin
+  fi
+  cd ..
+  cd deploy || exit
+  echo 'docker compose -f nuxt_admin.yml up -d'
+  docker compose -f nuxt_admin.yml up -d
+fi
+if [ "$(echo $works|grep 'nuxt_web')" != '' ]; then
   echo '##########开始构建nuxt_web##########'
   cd ..
   cd nuxt_web || exit
@@ -103,4 +103,4 @@ node --version
   cd ..
   cd deploy || exit
   docker compose -f nuxt_web.yml up -d
-#fi
+fi
