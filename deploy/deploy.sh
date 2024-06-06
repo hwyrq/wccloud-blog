2d51320bb5ab396428298017e0f439cdb57c9260
wcz <hwyrqw@163.com>
2024-06-06 19:02:20 +0800

    docker

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -18,42 +18,42 @@ works=$(git log -1 --stat |awk -F ' ' '{print $1}' |awk -F '/' '{print $1}' | aw
 echo "works: "
 echo $works
 #java
-if [ "$(echo $works|grep '#wccloud-admin#')" != "" ]; then
-  echo '##########开始构建wccloud-admin##########'
-  package
-  if [ "$(docker ps|grep wccloud-admin)" != "" ]; then
-    echo  'stop rm rmi '
-   docker stop wccloud-admin && docker rm wccloud-admin && docker rmi wccloud-admin
-  fi
-  docker compose -f wccloud-admin.yml up -d
-fi
-
-if [ "$(echo $works|grep '#wccloud-auth#')" != "" ]; then
-  echo '##########开始构建wccloud-auth##########'
-  package
-  if [ "$(docker ps|grep wccloud-auth)" != "" ]; then
-    echo  'stop rm rmi '
-   docker stop wccloud-auth && docker rm wccloud-auth && docker rmi wccloud-auth
-  fi
-  docker compose -f wccloud-auth.yml up -d
-fi
-
-if [ "$(echo $works|grep '#wccloud-gateway#')" != "" ]; then
-  echo '##########开始构建wccloud-gateway##########'
-  package
-  if [ "$(docker ps|grep wccloud-gateway)" != "" ]; then
-    echo  'stop rm rmi '
-   docker stop wccloud-gateway && docker rm wccloud-gateway && docker rmi wccloud-gateway
-  fi
-  docker compose -f wccloud-gateway.yml up -d
-fi
+#if [ "$(echo $works|grep '#wccloud-admin#')" != "" ]; then
+#  echo '##########开始构建wccloud-admin##########'
+#  package
+#  if [ "$(docker ps|grep wccloud-admin)" != "" ]; then
+#    echo  'stop rm rmi '
+#   docker stop wccloud-admin && docker rm wccloud-admin && docker rmi wccloud-admin
+#  fi
+#  docker compose -f wccloud-admin.yml up -d
+#fi
+#
+#if [ "$(echo $works|grep '#wccloud-auth#')" != "" ]; then
+#  echo '##########开始构建wccloud-auth##########'
+#  package
+#  if [ "$(docker ps|grep wccloud-auth)" != "" ]; then
+#    echo  'stop rm rmi '
+#   docker stop wccloud-auth && docker rm wccloud-auth && docker rmi wccloud-auth
+#  fi
+#  docker compose -f wccloud-auth.yml up -d
+#fi
+#
+#if [ "$(echo $works|grep '#wccloud-gateway#')" != "" ]; then
+#  echo '##########开始构建wccloud-gateway##########'
+#  package
+#  if [ "$(docker ps|grep wccloud-gateway)" != "" ]; then
+#    echo  'stop rm rmi '
+#   docker stop wccloud-gateway && docker rm wccloud-gateway && docker rmi wccloud-gateway
+#  fi
+#  docker compose -f wccloud-gateway.yml up -d
+#fi
 
 #rust
 if [ "$(echo $works|grep '#wccloud-web-rust#')" != "" ]; then
   echo '##########开始构建wccloud-web-rust##########'
   cd ..
   cd wccloud-web-rust || exit
-  /root/.cargo/bin/cargo  build --release
+  #/root/.cargo/bin/cargo  build --release
   if [ "$(docker ps|grep wccloud-web-rust)" != "" ]; then
     echo  'stop rm rmi '
    docker stop wccloud-web-rust && docker rm wccloud-web-rust && docker rmi wccloud-web-rust
