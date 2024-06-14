9e3c3c69cf82ecc0f5e9042cb8d55f691a9a5975
wcz <hwyrqw@163.com>
2024-06-14 23:13:37 +0800

    博客展示界面： 样式以及 目录

--- nuxt_web/pages/info/[id].vue
+++ nuxt_web/pages/info/[id].vue
@@ -1,8 +1,10 @@
 <template>
   <el-space style="align-items: normal">
+    <div  id="outline" style="position: fixed;top: 100px;left:200px"></div>
     <el-card style="width: 800px;margin-top: 10px;margin-bottom: 10px">
       <h1 v-if="blogData.title">{{ blogData.title }}</h1>
-      <div v-html="blogData.html"></div>
+      <div style="width: 100%;border: 1px solid #666666"></div>
+      <div id="preview" v-html="blogData.html"></div>
     </el-card>
   </el-space>
 </template>
@@ -11,11 +13,50 @@
 
 import {useAsyncData} from "#app";
 import {one} from "~/api";
+import 'vditor/dist/index.css'
+import Vditor from "vditor";
 
 const route = useRoute();
 const {data:{value:data}} = await useAsyncData(() => one({blogId: route.params.id}));
 const blogData = data.data;
 
+
+onMounted(async () => {
+
+  const outlineElement = document.getElementById('outline');
+  const previewElement = document.getElementById('preview');
+  Vditor.preview(previewElement, blogData.md, {
+    mode: "dark",
+    hljs: {style: "dracula"},
+    cdn: "/vditor",
+    markdown: {toc: true,mark:true},
+    speech: {
+      enable: true,
+    },
+    anchor: 1, after() {
+      Vditor.outlineRender(previewElement, outlineElement)
+      if (outlineElement.innerText.trim() !== '') {
+        outlineElement.style.display = 'block'
+      }
+    },
+
+  });
+
+  const outline_refresh = function () {
+    if (window.innerWidth <= 1200) {
+      outlineElement.style = "display: none;";
+    }else{
+      outlineElement.style = "display: block;position: fixed;top: 100px;left:"+(window.innerWidth/2-650)+"px";
+    }
+  };
+  onresize = function () {
+    outline_refresh();
+  };
+  outline_refresh();
+
+
+
+})
 const instance = getCurrentInstance();
 const siteInfo = instance?.attrs.siteInfo;
 
@@ -29,6 +70,6 @@ useHead({
 
 </script>
 
-<style>
+<style lang="css" scoped>
 
 </style>
