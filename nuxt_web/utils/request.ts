56d8eba46d1f810c1061aaaad9c3b117db4d4c9e
wcz <hwyrqw@163.com>
2024-06-20 20:01:26 +0800

    docker换自有的源

--- nuxt_web/utils/request.ts
+++ nuxt_web/utils/request.ts
@@ -5,8 +5,6 @@ import jsonBig from "json-bigint";
  * author wcz
  */
 const requester = axios.create({
-    //有一说一，这里真的坑，服务端请求用process.env.BASE_URL，客户端请求用useRuntimeConfig,反过来或者只用一个是不行的
-    //也可能自己不是专业前端吧，落伍了
     baseURL: process.env.BASE_URL || useRuntimeConfig().public.baseUrl,
     timeout: 15000,
     transformResponse: data => {
