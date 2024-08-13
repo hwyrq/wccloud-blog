b56cf1b25b442c29ee58e44f4a797de212d9a5e7
wcz <hwyrqw@163.com>
2024-08-13 22:35:00 +0800

    mybatis自动注入值先检测是否有该字段

--- wccloud-common/src/main/java/top/wccloud/common/MyMetaObjectHandler.java
+++ wccloud-common/src/main/java/top/wccloud/common/MyMetaObjectHandler.java
@@ -5,7 +5,13 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.ibatis.reflection.MetaObject;
 import org.springframework.stereotype.Component;
 
+import java.lang.reflect.Field;
 import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 /**
  * mybatis自动注入字段
  * @author wcz
@@ -13,18 +19,46 @@ import java.time.LocalDateTime;
 @Component
 @Slf4j
 public class MyMetaObjectHandler implements MetaObjectHandler {
+
+    public static Set<String> declaredFields(MetaObject metaObject) {
+        Field[] declaredFields = metaObject.getOriginalObject().getClass().getDeclaredFields();
+        Field[] declaredFields1 = metaObject.getOriginalObject().getClass().getSuperclass().getDeclaredFields();
+        Set<String> collect = Arrays.stream(declaredFields).map(Field::getName).collect(Collectors.toSet());
+        Set<String> collect1 = Arrays.stream(declaredFields1).map(Field::getName).collect(Collectors.toSet());
+        collect.addAll(collect1);
+        return collect;
+    }
+
     @Override
     public void insertFill(MetaObject metaObject) {
-        this.strictInsertFill(metaObject, "createTime", LocalDateTime::now, LocalDateTime.class);
-        this.strictInsertFill(metaObject, "updateTime", LocalDateTime::now, LocalDateTime.class);
-        this.strictInsertFill(metaObject, "createUserId", () -> 0L, Long.class);
-        this.strictInsertFill(metaObject, "updateUserId", () -> 0L, Long.class);
+        //先判断是否有该字段， 再添加值
+        Set<String> set = declaredFields(metaObject);
+
+        if (set.contains("createTime")) {
+            this.strictInsertFill(metaObject, "createTime", LocalDateTime::now, LocalDateTime.class);
+        }
+        if (set.contains("updateTime")) {
+            this.strictInsertFill(metaObject, "updateTime", LocalDateTime::now, LocalDateTime.class);
+        }
+        if (set.contains("createUserId")) {
+            this.strictInsertFill(metaObject, "createUserId", () -> 0L, Long.class);
+
+        }
+        if (set.contains("updateUserId")) {
+            this.strictInsertFill(metaObject, "updateUserId", () -> 0L, Long.class);
+
+        }
 
     }
 
     @Override
     public void updateFill(MetaObject metaObject) {
-        this.strictUpdateFill(metaObject, "updateTime", LocalDateTime::now, LocalDateTime.class);
-        this.strictUpdateFill(metaObject, "updateUserId", () -> 0L, Long.class);
+        Set<String> set = declaredFields(metaObject);
+        if (set.contains("updateTime")) {
+            this.strictUpdateFill(metaObject, "updateTime", LocalDateTime::now, LocalDateTime.class);
+        }
+        if (set.contains("updateUserId")) {
+            this.strictUpdateFill(metaObject, "updateUserId", () -> 0L, Long.class);
+        }
     }
 }
