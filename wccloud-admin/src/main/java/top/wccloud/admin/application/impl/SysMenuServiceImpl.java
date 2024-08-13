3195ee35ca1c69650e89ed42e900e9a85826666b
wcz <hwyrqw@163.com>
2024-08-13 22:56:18 +0800

    SysMenuService 添加spring cache

--- wccloud-admin/src/main/java/top/wccloud/admin/application/impl/SysMenuServiceImpl.java
+++ wccloud-admin/src/main/java/top/wccloud/admin/application/impl/SysMenuServiceImpl.java
@@ -1,5 +1,10 @@
 package top.wccloud.admin.application.impl;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import top.wccloud.admin.application.SysMenuService;
 import top.wccloud.admin.controller.vo.SysMenuVO;
 import top.wccloud.admin.doman.SysMenuRepository;
@@ -9,11 +14,14 @@ import jakarta.annotation.Resource;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+
 /**
  * @author wcz
  */
 @Service
+@CacheConfig(cacheNames = "SysMenuServiceImpl",keyGenerator = "cacheKeyGenerator")
 public class SysMenuServiceImpl implements SysMenuService {
+    private static final Logger log = LoggerFactory.getLogger(SysMenuServiceImpl.class);
     @Resource
     private SysMenuRepository sysMenuRepository;
 
@@ -22,26 +30,31 @@ public class SysMenuServiceImpl implements SysMenuService {
 
 
     @Override
+    @Cacheable(sync = true)
     public List<SysMenuVO> listTree0() {
         return SysMenuConvert.INSTANCE.convert1(sysMenuRepository.listTree0());
     }
 
     @Override
+    @Cacheable(sync = true)
     public List<SysMenuVO> listTree() {
         return SysMenuConvert.INSTANCE.convert1(sysMenuRepository.listTree());
     }
 
     @Override
+    @CacheEvict(allEntries = true)
     public Boolean save(SysMenuVO sysMenuVO) {
         return sysMenuRepository.save(SysMenuConvert.INSTANCE.convert2(sysMenuVO));
     }
 
     @Override
+    @CacheEvict(allEntries = true)
     public Boolean update(SysMenuVO sysMenuVO) {
         return sysMenuRepository.updateById(SysMenuConvert.INSTANCE.convert2(sysMenuVO));
     }
 
     @Override
+    @CacheEvict(allEntries = true)
     public Boolean delete(Long menuId) {
         return sysMenuRepository.removeById(menuId);
     }
